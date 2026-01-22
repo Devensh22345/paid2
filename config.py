ae9903da9ba2bef952fde4c6f6d1603f2a08a8f3
Devensh22345 <kumardevensh0@gmail.com>
2026-01-22 22:11:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,14 +5,14 @@ load_dotenv()
 
 class Config:
     # Telegram Bot Token
-    BOT_TOKEN = os.getenv("BOT_TOKEN")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "8446660054:AAEZbyTPJtZbe7XdIMJJc5w8Pk2Har0P5QE")
     
     # MongoDB Configuration
-    MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+    MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://6:6@cluster0.ne91zzc.mongodb.net/?appName=Cluster0")
     DATABASE_NAME = os.getenv("DATABASE_NAME", "telegram_bot_db")
     
     # Bot Owner ID (for initial setup)
-    OWNER_ID = int(os.getenv("OWNER_ID", 0))
+    OWNER_ID = int(os.getenv("OWNER_ID", "6872968794"))
     
     # Timezone for scheduling
     TIMEZONE = os.getenv("TIMEZONE", "UTC")
